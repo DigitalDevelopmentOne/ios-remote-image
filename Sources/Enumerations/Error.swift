d7bda444474bddfe58eac1b3c033583083d0edb1
cbepxbeo <cbepxbeo@mail.ru>
2023-08-15 05:19:41 +0300

    Update Error.swift

--- Sources/Enumerations/Error.swift
+++ Sources/Enumerations/Error.swift
@@ -11,11 +11,24 @@ import Foundation
 
 public enum Error: Swift.Error, CustomDebugStringConvertible {
     case incorrectUrl(message: String)
+    case preservation(message: String)
+    case imageAssembly(message: String)
+    case remoteData(error: Swift.Error)
+    case location(message: String)
     
     public var debugDescription: String {
+        let label = "RemoteImage Error: "
         switch self {
         case .incorrectUrl(let message):
-            return "RemoteImage Error: incorrect url (\(message)"
+            return "\(label)incorrect url (\(message))"
+        case .preservation(let message):
+            return "\(label)preservation (\(message))"
+        case .imageAssembly(let message):
+            return "\(label)image assembly (\(message))"
+        case .remoteData(let error):
+            return "\(label)remoteData (\(error))"
+        case .location(let message):
+            return "\(label) location (\(message))"
         }
     }
 }
