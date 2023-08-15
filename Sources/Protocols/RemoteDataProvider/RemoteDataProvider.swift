fa6f5d862567b5853ed23198085300df2d0090ed
cbepxbeo <cbepxbeo@mail.ru>
2023-08-15 05:20:04 +0300

    update RemoteDataProvider

---
+++ Sources/Protocols/RemoteDataProvider/RemoteDataProvider.swift
@@ -0,0 +1,16 @@
+/*
+ 
+ Project: RemoteImage
+ File: RemoteDataProvider.swift
+ Created by: Egor Boyko
+ Date: 14.08.2023
+ 
+ */
+
+import Foundation
+
+public protocol RemoteDataProvider {
+    init()
+    var delegate: RemoteDataProviderDelegate? { get set }
+    func download(url: URL, handler: @escaping (Result<Data, Error>) -> Void)
+}
