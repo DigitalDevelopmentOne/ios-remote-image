9b49209d7d472c2e38c124eeeebcbc15bfd58c6f
cbepxbeo <cbepxbeo@mail.ru>
2023-08-15 04:02:54 +0300

    Update Handler

---
+++ Sources/Protocols/Handler/Handler.swift
@@ -0,0 +1,15 @@
+/*
+ 
+ Project: RemoteImage
+ File: Handler.swift
+ Created by: Egor Boyko
+ Date: 15.08.2023
+ 
+ */
+
+import Foundation
+
+public protocol Handler {
+    func transform(from url: URL) -> String
+    init()
+}
