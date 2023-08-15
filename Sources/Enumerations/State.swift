91f4e90dca1d9aa942fd238f264aa9dc25fa84c8
cbepxbeo <cbepxbeo@mail.ru>
2023-08-15 03:39:21 +0300

    Add cases to State

---
+++ Sources/Enumerations/State.swift
@@ -0,0 +1,17 @@
+/*
+ 
+ Project: RemoteImage
+ File: State.swift
+ Created by: Egor Boyko
+ Date: 14.08.2023
+ 
+ */
+
+import UIKit
+
+public enum State {
+    case fetch(progress: CGFloat)
+    case inaction
+    case success(image: UIImage)
+    case failure(error: Error)
+}
