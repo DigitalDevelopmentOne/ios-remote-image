c6d517d772cc6bb109b4db674755d8866930dcab
cbepxbeo <cbepxbeo@mail.ru>
2023-08-16 03:39:44 +0300

    Create RemoteImage+Modifiers.swift

---
+++ Sources/RemoteImage/RemoteImage+Modifiers.swift
@@ -0,0 +1,23 @@
+/*
+ 
+ Project: RemoteImage
+ File: RemoteImage+Modifiers.swift
+ Created by: Egor Boyko
+ Date: 16.08.2023
+ 
+ */
+
+
+extension RemoteImage {
+    public func configuration(_ value: Configuration) -> Self {
+        var view = self
+        view.configuration = configuration
+        return view
+    }
+    
+    public func force(_ value: Bool) -> Self {
+        var view = self
+        view.force = value
+        return view
+    }
+}
